8cec5ba0ef1dda25196fa5bbb920e57e2e9efd8d
visotoniki9 <d.simon.ssx0@gmail.com>
2021-05-25 21:40:09 +0200

    Finished Header

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': 'off',
+    'react/self-closing-comp': 'off',
   },
 };
---
+++ components/Avatar.jsx
@@ -0,0 +1,12 @@
+function Avatar({ url }) {
+  return (
+    <img
+      className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-125"
+      loading="loading"
+      src={url}
+      alt="profile pic"
+    />
+  );
+}
+
+export default Avatar;
