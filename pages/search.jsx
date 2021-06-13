1953a43f6be425ef32d4c250a109ee551e85529a
visotoniki9 <d.simon.ssx0@gmail.com>
2021-06-13 23:13:58 +0200

    Added cache for api requests

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
+import cache from 'memory-cache';
 import Header from '../components/Header';
 
-function Search() {
+const { GOOGLE_API_KEY, GOOGLE_CONTEXT_KEY } = process.env;
+
+function Search({ results }) {
   return (
     <div>
       <Head>
@@ -15,3 +18,30 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const reqUrl = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${GOOGLE_CONTEXT_KEY}&q=${context.query.term}`;
+
+  const cachedResponse = cache.get(reqUrl);
+
+  if (cachedResponse) {
+    console.log('Loaded from cache');
+    return {
+      props: {
+        results: cachedResponse,
+      },
+    };
+  }
+
+  console.log('Not found in cache');
+  const response = await fetch(reqUrl);
+  const data = await response.json();
+  const hours = 24;
+  cache.put(reqUrl, data, hours * 24 * 60 * 60);
+
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
